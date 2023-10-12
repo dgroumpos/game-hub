0ab3f9e8e3d94d99304a3a2571f2f79dc18bb572
Dimitris Groumpos <groumposd@unisystems.gr>
2023-10-12 08:38:16 +0300

    implement infinite queries

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,21 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
-import genres from "../data/genres";
-import APIClient from "../services/api-client";
+import { useQuery } from '@tanstack/react-query';
+import genres from '../data/genres';
+import APIClient from '../services/api-client';
 
-const apiClient = new APIClient<Genre>("/genres");
+const apiClient = new APIClient<Genre>('/genres');
 
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
-const useGenres = () => {
-  return useQuery({
-    queryKey: ["genres"],
+
+const useGenres = () =>
+  useQuery({
+    queryKey: ['genres'],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
-    initialData: { count: genres.length, results: genres },
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: { count: genres.length, results: genres }
   });
-};
 
 export default useGenres;
